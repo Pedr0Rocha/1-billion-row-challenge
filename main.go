6f125d3493fd83d56a2d38545950e8d0b06191fe
Pedr0Rocha <pedrovqrocha@gmail.com>
2024-04-09 21:08:52 +0200

    implement concurrency

--- main.go
+++ main.go
@@ -11,14 +11,18 @@ import (
 	"runtime/pprof"
 	"sort"
 	"strconv"
+	"sync"
 	"unsafe"
 )
 
 const (
 	MAX_STATIONS = 10_000
 	// on an average of 16 bytes per row, we now read ~1M rows at once
+	// @TODO: experiment buffer sizes
 	CHUNK_SIZE = 16 * 1024 * 1024
 )
+
+type StationMap map[string]*stationData
 
 type stationData struct {
 	min   int
@@ -71,64 +75,87 @@ func main() {
 }
 
 func processFile(file *os.File, chunkSize int) string {
-	readBuffer := make([]byte, chunkSize)
-	var leftoverBuffer []byte
+	var wg sync.WaitGroup
+	nWorkers := runtime.NumCPU() - 1
 
-	stations := make(map[string]*stationData, MAX_STATIONS)
-	for {
-		readBytes, err := file.Read(readBuffer)
-		if err == io.EOF {
-			// read the whole file
-			break
+	// @TODO: experiment with buffered channels
+	resultsChannel := make(chan StationMap)
+	chunkBufferChannel := make(chan []byte)
+
+	// read chunks of bytes and send it over in the chunkBufferChannel
+	go func() {
+		readBuffer := make([]byte, chunkSize)
+		var leftoverBuffer []byte
+		for {
+			readBytes, err := file.Read(readBuffer)
+			if err == io.EOF {
+				// read the whole file
+				break
+			}
+			if readBytes == 0 {
+				log.Fatal("could not read file")
+			}
+
+			readBuffer = readBuffer[:readBytes]
+
+			// find last new line to set a stopping point where we have complete rows
+			lastLineIndex := bytes.LastIndex(readBuffer, []byte("\n"))
+
+			// walk until the last new line +1 to consume it
+			// here we need to merge the last leftover with the current buffer
+			resultBuffer := make([]byte, len(leftoverBuffer)+readBytes)
+			resultBuffer = append(leftoverBuffer, readBuffer[:lastLineIndex+1]...)
+
+			// prepare the leftovers to be used in the next iteration
+			// which is everything that we left out when finding the last new line
+			leftoverBuffer = make([]byte, len(readBuffer[lastLineIndex+1:]))
+			copy(leftoverBuffer, readBuffer[lastLineIndex+1:])
+
+			chunkBufferChannel <- resultBuffer
 		}
-		if readBytes == 0 {
-			log.Fatal("could not read file")
-		}
+		close(chunkBufferChannel)
+		// waiting for all `parseChunk` calls to finish, meaning we have a result channel ready
+		wg.Wait()
+		close(resultsChannel)
+	}()
 
-		readBuffer = readBuffer[:readBytes]
+	for range nWorkers {
+		wg.Add(1)
+		go func() {
+			// will read until the chunk channel closes
+			for chunk := range chunkBufferChannel {
+				parseChunk(chunk, resultsChannel)
+			}
+			wg.Done()
+		}()
+	}
 
-		// find last new line to set a stopping point where we have complete rows
-		lastLineIndex := bytes.LastIndex(readBuffer, []byte("\n"))
-
-		// walk until the last new line +1 to consume it
-		// here we need to merge the last leftover with the current buffer
-		resultBuffer := make([]byte, len(leftoverBuffer)+readBytes)
-		resultBuffer = append(leftoverBuffer, readBuffer[:lastLineIndex+1]...)
-
-		// prepare the leftovers to be used in the next iteration
-		// which is everything that we left out when finding the last new line
-		leftoverBuffer = make([]byte, len(readBuffer[lastLineIndex+1:]))
-		copy(leftoverBuffer, readBuffer[lastLineIndex+1:])
-
-		for len(resultBuffer) != 0 {
-			newBuffer, stationName, measurement := parseBufferSingleRow(resultBuffer)
-			resultBuffer = newBuffer
-
-			measurementInt, _ := strconv.Atoi(bytesToString(measurement))
-			station := bytesToString(stationName)
-
-			data, exist := stations[station]
+	// process all the station maps coming from the parseChunk workers
+	stations := make(StationMap, MAX_STATIONS)
+	for result := range resultsChannel {
+		for st, data := range result {
+			existingData, exist := stations[st]
 			if !exist {
 				stationData := stationData{
-					min:   measurementInt,
-					max:   measurementInt,
-					sum:   measurementInt,
-					count: 1,
+					min:   data.min,
+					max:   data.max,
+					sum:   data.sum,
+					count: data.count,
 				}
 
-				stations[station] = &stationData
+				stations[st] = &stationData
 				continue
 			}
 
-			if data.min > measurementInt {
-				data.min = measurementInt
+			if existingData.min > data.min {
+				existingData.min = data.min
 			}
-			if data.max < measurementInt {
-				data.max = measurementInt
+			if existingData.max < data.max {
+				existingData.max = data.max
 			}
 
-			data.count++
-			data.sum += measurementInt
+			existingData.count += data.count
+			existingData.sum += data.sum
 		}
 	}
 
@@ -158,10 +185,45 @@ func processFile(file *os.File, chunkSize int) string {
 	return resultStr
 }
 
+func parseChunk(resultBuffer []byte, resultsChan chan<- StationMap) {
+	stations := make(StationMap, MAX_STATIONS)
+	for len(resultBuffer) != 0 {
+		newBuffer, stationName, measurement := parseRow(resultBuffer)
+		resultBuffer = newBuffer
+
+		measurementInt, _ := strconv.Atoi(bytesToString(measurement))
+		station := bytesToString(stationName)
+
+		data, exist := stations[station]
+		if !exist {
+			stationData := stationData{
+				min:   measurementInt,
+				max:   measurementInt,
+				sum:   measurementInt,
+				count: 1,
+			}
+
+			stations[station] = &stationData
+			continue
+		}
+
+		if data.min > measurementInt {
+			data.min = measurementInt
+		}
+		if data.max < measurementInt {
+			data.max = measurementInt
+		}
+
+		data.count++
+		data.sum += measurementInt
+	}
+	resultsChan <- stations
+}
+
 // parses the buffer to extract station name and measurement from a single row
 // also returns the remainder bytes of the buffer
 // can most likely be modified to return byte interval positions, name[0:20], measurement[21:25]
-func parseBufferSingleRow(resultBuffer []byte) ([]byte, []byte, []byte) {
+func parseRow(resultBuffer []byte) ([]byte, []byte, []byte) {
 	cursor := -1
 	var stationName []byte
 	for i, char := range resultBuffer {
