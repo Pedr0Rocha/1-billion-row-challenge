22cde5a19f7df40f279d99c37fff107fb2f6503c
Pedr0Rocha <pedrovqrocha@gmail.com>
2024-04-09 16:46:09 +0200

    add test case with 1m entries to start experimenting concurrency

--- main_test.go
+++ main_test.go
@@ -5,19 +5,31 @@ import (
 	"testing"
 )
 
-const (
-	// small enough to have leftovers
-	TEST_CHUNK_SIZE = 32
-)
-
 func TestMain(t *testing.T) {
-	t.Run("should return correct output", func(t *testing.T) {
+	t.Run("should return correct output for small dataset", func(t *testing.T) {
 		input, _ := os.Open("test_data/input.txt")
 		defer input.Close()
 		expected, _ := os.ReadFile("test_data/expected.txt")
 		expectedContents := string(expected)
 
-		result := processFile(input, TEST_CHUNK_SIZE)
+		testChunkSize := 32
+
+		result := processFile(input, testChunkSize)
+
+		if result != expectedContents {
+			t.Errorf("wrong result. \nexpected:%s, \ngot:%s", expectedContents, result)
+		}
+	})
+
+	t.Run("should return correct output for 1M entries dataset", func(t *testing.T) {
+		input, _ := os.Open("test_data/input-1m.txt")
+		defer input.Close()
+		expected, _ := os.ReadFile("test_data/expected-1m.txt")
+		expectedContents := string(expected)
+
+		testChunkSize := 1024 * 512
+
+		result := processFile(input, testChunkSize)
 
 		if result != expectedContents {
 			t.Errorf("wrong result. \nexpected:%s, \ngot:%s", expectedContents, result)
